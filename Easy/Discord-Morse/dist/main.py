84f95165aa831c6810a999572bcbe10c16efefa1
ManavMuthanna <muthannamanav2003@gmail.com>
2022-09-09 15:22:40 +0530

    modified:   Easy/Discord-Morse/README.md 	new file:   Easy/Discord-Morse/dist/.env 	deleted:    Easy/Discord-Morse/dist/discord_bot.py 	new file:   Easy/Discord-Morse/dist/main.py 	modified:   Easy/Discord-Morse/dist/morse_code.txt 	modified:   Easy/Discord-Morse/dist/youtube_video.mp4

---
+++ Easy/Discord-Morse/dist/main.py
@@ -0,0 +1,59 @@
+import dotenv
+from dotenv import load_dotenv
+import os
+import discord
+from discord.ext import commands
+import interactions
+
+server_id = 1017708756676644935
+
+
+load_dotenv()
+token = os.getenv('TOKEN')
+bot = interactions.Client(token)
+
+
+@bot.event
+async def on_ready():
+    print("Ready!")
+
+# hello command
+
+
+@bot.command(
+    name="hello",
+    description="An example command",
+    scope=server_id,
+)
+async def hello(ctx: interactions.CommandContext, user: discord.Member = None):
+    if user == None:
+        user = ctx.author
+        await ctx.send("Check DM!", ephemeral=True)
+        await user.send("Hello There!")
+
+
+# youtube link command
+@bot.command(
+    name="answer",
+    description="Maybe Happy will give you a reward?",
+    scope=server_id,
+    options=[
+        interactions.Option(
+            type=interactions.OptionType.STRING,
+            name="answer",
+            description="Input your answer"
+        ),
+    ],
+)
+async def say(ctx: interactions.CommandContext, user: discord.Member = None, *, answer=str):
+    if user == None:
+        user = ctx.author
+        if (answer == "Hi"):
+            await ctx.send("Correct Answer!", ephemeral=True)
+            await user.send("https://youtu.be/_dkFaEcR9ig")
+
+        else:
+            await ctx.send("Wrong Answer!", ephemeral=True)
+
+
+bot.start()
